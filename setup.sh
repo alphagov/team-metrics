4ef8de88c9f9abfb802eba8a2125dbd54e63bbdb
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-11-09 16:51:45 +0000

    add a script to setup the environment

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# use pip3 to install virtualenv, and use pip later because it's the right one at that point
+pip3 install virtualenv
+virtualenv -p /usr/local/bin/python3.7 venv
+source venv/bin/activate
+
+pip install -r requirements.txt
