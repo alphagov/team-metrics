211b3fa1eaac2c9ee1fcb7b26d2b42f819710a51
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2018-11-12 14:13:59 +0000

    Move Jira metrics to own function

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def get_dict_from_key_value_pair(input):
 def number_of_stories_completed(jira, project, sprint):
     for issue in jira.search_issues('project={} AND SPRINT in ({})'.format(project.id, sprint.id)):
         # print("  issue whole: {}".format(issue.raw))
+
         # need to parse customfield_10020 for ACTIVE, startDate and endDate
         # don't always use the first entry!
         cf = issue.fields.customfield_10020[0]
@@ -30,19 +31,25 @@ def number_of_stories_completed(jira, project, sprint):
             print("more than one customfield_10020!")
         try:
             fields = get_dict_from_key_value_pair(cf)
-            print("  issue: {}, {}, state: {} - startDate: {} - endDate: {} - completeDate: {}".format(
-                issue.id,
-                issue.key,
+            print("  issue: {} - {}".format(issue.id, issue.key))
+            print("    state: {} - startDate: {} - endDate: {} - completeDate: {}".format(
                 fields['state'],
                 fields['startDate'],
                 fields['endDate'],
                 fields['completeDate'])
             )
+
+            issue = jira.issue(issue.id, expand='changelog')
+            changelog = issue.changelog
+            for history in changelog.histories:
+                for item in history.items:
+                    if item.field == 'status':
+                        print('    Date:' + history.created + ' From:' + item.fromString + ' To:' + item.toString)
         except ValueError as e:
             pass
 
 
-def main():
+def get_jira_metrics():
     jira = JIRA(
         basic_auth=(os.environ['TM_USER'], os.environ['TM_PAT']),
         server='https://{}.atlassian.net'.format(os.environ['TM_HOST']),
@@ -69,6 +76,9 @@ def main():
                 # print(e)
                 pass
 
+
+def main():
+    get_jira_metrics()
 
 
 if __name__ == "__main__":
