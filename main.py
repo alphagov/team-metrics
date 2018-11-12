b5b1c381564233dcdd68f9bf86b043ade8c4e0d8
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2018-11-12 15:56:16 +0000

    Updated the code to use a class, and code to find the field name

--- main.py
+++ main.py
@@ -7,78 +7,110 @@ from jira.exceptions import JIRAError
 from jira.resources import GreenHopperResource
 
 
-def cycle_time(issue):
-    pass
+class Jira(object):
+    sprint_info_field_name = ''
 
+    def __init__(self):
+        self.jira = JIRA(
+            basic_auth=(os.environ['TM_USER'], os.environ['TM_PAT']),
+            server='https://{}.atlassian.net'.format(os.environ['TM_HOST']),
+            options={'agile_rest_path': GreenHopperResource.AGILE_BASE_REST_PATH}
+        )
 
-def process_cycle_efficiency(issue):
-    pass
+    def cycle_time(self, fields):
+        print("  cycle time")
+        print("    state: {} - startDate: {} - endDate: {} - completeDate: {}".format(
+            fields['state'],
+            fields['startDate'],
+            fields['endDate'],
+            fields['completeDate'])
+        )
 
+    def process_cycle_efficiency(self, issue):
+        print("  process cycle efficiency")
+        issue = self.jira.issue(issue.id, expand='changelog')
+        changelog = issue.changelog
+        for history in changelog.histories:
+            for item in history.items:
+                if item.field == 'status':
+                    print('    Date:' + history.created + ' From:' + item.fromString + ' To:' + item.toString)
 
-def get_dict_from_key_value_pair(input):
-    key_value_str = re.search(r"\[(.+)\]", input).group(1)
-    return dict(kv.split("=") for kv in key_value_str.split(","))
-
-
-def number_of_stories_completed(jira, project, sprint):
-    for issue in jira.search_issues('project={} AND SPRINT in ({})'.format(project.id, sprint.id)):
-        # print("  issue whole: {}".format(issue.raw))
-
-        # need to parse customfield_10020 for ACTIVE, startDate and endDate
-        # don't always use the first entry!
-        cf = issue.fields.customfield_10020[0]
-        if len(issue.fields.customfield_10020) > 1:
-            print("more than one customfield_10020!")
+    def get_dict_from_key_value_pair(self, input):
+        kv_matches = re.search(r"\[(.+)\]", input)
+        if not kv_matches:
+            return None
+        kv_string = kv_matches.group(1)
         try:
-            fields = get_dict_from_key_value_pair(cf)
-            print("  issue: {} - {}".format(issue.id, issue.key))
-            print("    state: {} - startDate: {} - endDate: {} - completeDate: {}".format(
-                fields['state'],
-                fields['startDate'],
-                fields['endDate'],
-                fields['completeDate'])
-            )
-
-            issue = jira.issue(issue.id, expand='changelog')
-            changelog = issue.changelog
-            for history in changelog.histories:
-                for item in history.items:
-                    if item.field == 'status':
-                        print('    Date:' + history.created + ' From:' + item.fromString + ' To:' + item.toString)
+            kv_dict = dict(kv.split("=") for kv in kv_string.split(","))
+            return dict((key, None if value == "<null>" else value) for key, value in kv_dict.items())
         except ValueError as e:
-            pass
+            print("###### ", e, input)
+            return None
 
+    def get_jira_metrics(self):
+        # # Get all projects viewable by anonymous users.
+        projects = self.jira.projects()
 
-def get_jira_metrics():
-    jira = JIRA(
-        basic_auth=(os.environ['TM_USER'], os.environ['TM_PAT']),
-        server='https://{}.atlassian.net'.format(os.environ['TM_HOST']),
-        options={'agile_rest_path': GreenHopperResource.AGILE_BASE_REST_PATH}
-    )
+        for project in projects:
+            sprints_found = False
+            boards = self.jira.boards(projectKeyOrID=project.id)
+            for board in boards:
+                print("board:", board)
+                try:
+                    for sprint in self.jira.sprints(board.id):
+                        if sprint.raw['state'] == 'future':
+                            continue
+                        sprints_found = True
+                        stories_completed = 0
 
-    # # Get all projects viewable by anonymous users.
-    projects = jira.projects()
+                        print("\nsprint: {}, {}, {} - {}".format(
+                            sprint.id, sprint.name, sprint.raw['startDate'], sprint.raw['endDate'])
+                        )
+                        for issue in self.jira.search_issues(
+                            'project={} AND SPRINT in ({})'.format(project.id, sprint.id)
+                        ):
+                            # print("*** issue whole: {}".format(issue.raw)); break
 
-    for project in projects:
-        boards = jira.boards(projectKeyOrID=project.id)
-        for board in boards:
-            print("board:", board)
-            try:
-                # print("sprints", jira.sprints(board.id))
-                for sprint in jira.sprints(board.id):
-                    if sprint.raw['state'] == 'future':
-                        continue
-                    print("sprint: {}, {}, {} - {}".format(sprint.id, sprint.name, sprint.raw['startDate'], sprint.raw['endDate']))
-                    number_of_stories_completed(jira, project, sprint)
-                    # print("sprint whole: {}".format(sprint.raw))
+                            if not self.sprint_info_field_name:
+                                for name in dir(issue.fields):
+                                    sprint_info = getattr(issue.fields, name)
+                                    if sprint_info and type(sprint_info) == list and \
+                                        'com.atlassian.greenhopper.service.sprint.Sprint' in sprint_info[0]:
+                                        self.sprint_info_field_name = name
 
-            except JIRAError as e:
-                # print(e)
-                pass
+                            if not self.sprint_info_field_name:
+                                continue
+
+                            sprint_info = getattr(issue.fields, self.sprint_info_field_name)
+                            if len(sprint_info) > 1:
+                                print("******** more than one sprint_info field!")
+                                print(sprint_info)
+
+                            cf = sprint_info[0]
+                            fields = self.get_dict_from_key_value_pair(cf)
+
+                            if not fields:
+                                print("######## no fields available: ", cf)
+                                continue
+
+                            print("  issue: {} - {}".format(issue.id, issue.key))
+                            self.cycle_time(fields)
+                            self.process_cycle_efficiency(issue)
+
+                            if fields['state'] == 'CLOSED':
+                                stories_completed += 1
+                        print("  stories completed: ", str(stories_completed))
+
+                except JIRAError as e:
+                    # print(e)
+                    pass
+            if sprints_found:
+                break
 
 
 def main():
-    get_jira_metrics()
+    jira = Jira()
+    jira.get_jira_metrics()
 
 
 if __name__ == "__main__":
