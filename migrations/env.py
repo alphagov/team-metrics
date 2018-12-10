dc16e0f7a49e633c9ead7586f05eb6fccfab4d81
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2018-12-10 13:59:55 +0000

    Update sqlalchemy.url with SQLALCHEMY_DATABASE_URI env var

--- migrations/env.py
+++ migrations/env.py
@@ -1,7 +1,11 @@
 from __future__ import with_statement
+import os
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+
+from app import models
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -13,8 +17,11 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app import models
 target_metadata = models.Base.metadata
+
+# this will overwrite the ini-file sqlalchemy.url path
+# with the path given in the config of the main code
+config.set_main_option('sqlalchemy.url', os.environ.get('SQLALCHEMY_DATABASE_URI'))
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
