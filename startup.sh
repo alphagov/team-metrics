d2fe444837dc6d0da13a4d8955421929a65ac5ad
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-12-10 10:30:29 +0000

    should start the db properly now

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
 if [ ! -d node_modules ]; then
     echo "====> installing frontend assets"
@@ -25,6 +25,7 @@ fi
 command -v docker > /dev/null || (echo "docker not installed" && exit 1)
 echo "====> starting database"
 DB_CONTAINER_NAME="team-metrics-dev-postgres"
+PYTHONPATH=$(pwd)
 
 function stop_database {
     docker stop "$DB_CONTAINER_NAME" >/dev/null 2>&1 || echo -n ''
@@ -32,9 +33,9 @@ function stop_database {
 trap stop_database EXIT
 
 stop_database
-docker run --name "$DB_CONTAINER_NAME" --rm -p 5432:5432 postgres >log/postgres.log 2>&1 &
+mkdir -p log
+docker run -e "POSTGRES_DB=team_metrics" --name "$DB_CONTAINER_NAME" --rm -p 5432:5432 postgres >log/postgres.log 2>&1 &
 
-sqlalchemy.url=
 alembic revision --autogenerate
 alembic upgrade head
 
