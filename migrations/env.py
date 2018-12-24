51032ee9a4d8bd76b8689f290ed69600bb004c1f
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2018-12-24 15:48:20 +0000

    Enable alembic to find project root

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,12 @@
 from __future__ import with_statement
 import os
+import sys
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
+# allow app folder to be found
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
 from app import models
 
 
