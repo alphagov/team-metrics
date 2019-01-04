8035a7906e0ed422d2ff38d99520807ee4523601
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-01-04 15:12:35 +0000

    Fix startup.sh to work with docker

--- startup.sh
+++ startup.sh
@@ -24,23 +24,22 @@ fi
 
 command -v psql > /dev/null || (echo "psql not installed" && NO_PSQL=1);
 
-if [[ $NO_PSQL == 1 || $SQLALCHEMY_DATABASE_URI == 'docker' ]]; then
+if [[ $NO_PSQL == 1 || $USE_DOCKER == 'true' ]]; then
     command -v docker > /dev/null || (echo "docker not installed" && exit 1)
     echo "====> creating database in docker"
     DB_CONTAINER_NAME="team-metrics-dev-postgres"
-    export PYTHONPATH=$(pwd)
-    
     function stop_database {
         docker stop "$DB_CONTAINER_NAME" >/dev/null 2>&1 || echo -n ''
     }
     trap stop_database EXIT
     
+    export SQLALCHEMY_DATABASE_URI=postgresql://postgres@localhost:5432/team_metrics
     stop_database
     mkdir -p log
     docker run -e POSTGRES_DB=team_metrics --name "$DB_CONTAINER_NAME" --rm -p 5432:5432 postgres >log/postgres.log 2>&1 &
 else
     if [ -z "$SQLALCHEMY_DATABASE_URI" ]; then
-        echo "Please export SQLALCHEMY_DATABASE_URI, normally postgres://localhost:5432 or set to 'docker' to use docker postgres"
+        echo "Please export SQLALCHEMY_DATABASE_URI, normally postgres://localhost:5432 or 'export USE_DOCKER=true' to use docker postgres"
         exit
     else
         if psql -lqt | cut -d \| -f 1 | grep -qw ${SQLALCHEMY_DATABASE_URI##*/}; then
