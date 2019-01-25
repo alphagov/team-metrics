6dc449880b644ef4a73421a2c9f3526444f5483d
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-01-25 11:49:33 +0000

    Use github search to find repos

--- app/source/github.py
+++ app/source/github.py
@@ -21,82 +21,70 @@ class Github(Base):
 
         self.gh = login(token=os.environ['TM_GITHUB_PAT'])
 
-    def get_teams(self):
-        for org in self.gh.organizations():
-            print("Processing {} teams".format(org.url))
-            for team in org.teams():
-                print(team.id, team.name)
-
     def get_metrics(self, year=None, quarter=None):
         i = 0
-        metrics = []
         q_start, q_end = get_quarter_daterange(year, quarter)
         repos_yml = None
 
         with open('git-repos.yml') as f:
             repos_yml = yaml.load(f)
 
-        org = self.gh.organization("alphagov")
-        repositories = org.team(self.team_id).repositories()
-        for repo in repositories:
-            if repo.name not in repos_yml['observe']['repos'].split(' '):
-                continue
+        for yml_repo in repos_yml['observe']['repos'].split(' '):
+            search_results = self.gh.search_repositories(query=f'archived:false org:alphagov {yml_repo} in:name')
 
-            prs = repo.pull_requests(state="closed")
+            for repo in [r.repository for r in search_results]:
+                prs = repo.pull_requests(state="closed")
 
-            # One of our repos had no prs, for some reason this caused the program to crash
-            # There may be a better way to handle this
-            try:
-                prs.next()
-            except NotFoundError:
-                continue
+                # One of our repos had no prs, for some reason this caused the program to crash
+                # There may be a better way to handle this
+                try:
+                    prs.next()
+                except NotFoundError:
+                    continue
 
-            for pr in prs:
-                # if repo.name == 'prometheus-aws-configuration-beta' and pr.number == 190:
-                #     import pdb; pdb.set_trace()
-                # else:
-                #     continue
+                for pr in prs:
+                    # if repo.name == 'prometheus-aws-configuration-beta' and pr.number == 190:
+                    #     import pdb; pdb.set_trace()
+                    # else:
+                    #     continue
 
-                if pr.merged_at and q_start <= pr.merged_at.replace(tzinfo=None) <= q_end:
-                    print(repo.name, pr.created_at, pr.title)
+                    if pr.merged_at and q_start <= pr.merged_at.replace(tzinfo=None) <= q_end:
+                        print(repo.name, pr.title, pr.number)
 
-                    pr_date = pr.created_at.replace(tzinfo=None)
+                        pr_date = pr.created_at.replace(tzinfo=None)
 
-                    diff_before_pr = diff_after_pr = 0
-                    for c in pr.commits():
-                        commit_json = c.as_dict()
-                        commit_datetime = datetime.strptime(commit_json['commit']['author']['date'].replace("Z", ""), "%Y-%m-%dT%H:%M:%S")
+                        diff_before_pr = diff_after_pr = 0
+                        for c in pr.commits():
+                            commit_json = c.as_dict()
+                            commit_datetime = datetime.strptime(commit_json['commit']['author']['date'].replace("Z", ""), "%Y-%m-%dT%H:%M:%S")
 
-                        for f in repo.commit(c.sha).files:
-                            if commit_datetime < pr_date:
-                                diff_before_pr += f['additions'] + f['deletions']
-                            else:
-                                diff_after_pr += f['additions'] + f['deletions']
+                            for f in repo.commit(c.sha).files:
+                                if commit_datetime < pr_date:
+                                    diff_before_pr += f['additions'] + f['deletions']
+                                else:
+                                    diff_after_pr += f['additions'] + f['deletions']
 
-                    total_diff = diff_before_pr + diff_after_pr
-                    if total_diff == 0:
-                        rework = 0
-                    else:
-                        rework = (diff_after_pr / total_diff) * 100
-                    print('total diff', total_diff)
-                    print('diff after PR', diff_after_pr)
-                    print('total effort rework: {0:.2f}%'.format(rework))
+                        total_diff = diff_before_pr + diff_after_pr
+                        rework = (diff_after_pr / total_diff) * 100 if total_diff > 0 else 0
+                        print('total diff', total_diff)
+                        print('diff after PR', diff_after_pr)
+                        print('total effort rework: {0:.2f}%'.format(rework))
 
-                    full_pr = repo.pull_request(pr.number)
+                        full_pr = repo.pull_request(pr.number)
 
-                    dao_upsert_git_metric({
-                        'team_id': self.team_id,
-                        'name': repo.name,
-                        'pr_number': pr.number,
-                        'start_date': pr.created_at,
-                        'end_date': pr.merged_at,
-                        'diff_count': diff_after_pr,
-                        'total_diff_count': total_diff,
-                        'comments_count': full_pr.comments_count + full_pr.review_comments_count,
-                    })
-                    
-                    i += 1
-                # break
-            # if i > 10:
-            #     break
+                        dao_upsert_git_metric({
+                            'team_id': self.team_id,
+                            'name': repo.name,
+                            'pr_number': pr.number,
+                            'start_date': pr.created_at,
+                            'end_date': pr.merged_at,
+                            'diff_count': diff_after_pr,
+                            'total_diff_count': total_diff,
+                            'comments_count': full_pr.comments_count + full_pr.review_comments_count,
+                        })
+                        
+                        i += 1
+                    # break
+                # if i > 10:
+                #     break
         print(f'Number upserted: {i}')
