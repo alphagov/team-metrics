312fb807b5cfbed2162dc7718df17c0e56ae2ae1
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-01-28 11:27:04 +0000

    Add get_metrics helper for different sources

---
+++ app/source/metrics.py
@@ -0,0 +1,18 @@
+from app.source import NotFound
+from app.source.jira import Jira
+from app.source.pivotal import Pivotal
+from app.source.trello import Trello
+
+
+def get_metrics(source_type, _id, year, quarter):
+    if source_type == 'jira':
+        j = Jira(project_id=_id)
+        return j.get_metrics(year, quarter)
+    elif source_type == 'pivotal':
+        p = Pivotal(project_id=_id)
+        return p.get_metrics(year, quarter)
+    elif source_type == 'trello':
+        t = Trello(board_id=_id)
+        return t.get_metrics(year, quarter)
+    else:
+        raise NotFound(f"Source {source_type} not found")
